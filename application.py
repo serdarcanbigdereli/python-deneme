cc72819b7e33e3a2164ad188831fec23153f4130
root <root@LT-SERDARCANBUYUKDERELI.bites.local>
2021-11-25 13:00:09 +0300

    asd

--- application.py
+++ application.py
@@ -1,9 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def index():
     return 'Web App with Python Flask!'
 
-app.run(host='0.0.0.0', port=80)
+application.run(host='0.0.0.0', port=80)
